c8be6bffbe75ff2850d7ae55e7d1b6932fc1b917
Gabriel Brandao <gabrielbrandaosales@hotmail.com>
2025-04-15 20:40:48 -0300

    feat: constructor created & findAll / findOne created

--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TodoEntity } from './entity/todo.entity';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(
+    @InjectRepository(TodoEntity)
+    private readonly todoRepository: Repository<TodoEntity>,
+  ) {}
+
+  async findAll() {
+    return await this.todoRepository.find();
+  }
+
+  async findOne(id: string) {
+    try {
+      return await this.todoRepository.findOneOrFail({ where: { id } });
+    } catch (error) {
+      const message = error?.message || 'Todo não encontrado';
+      throw new NotFoundException(message);
+    }
+  }
+
+  async create() {}
+
+  async update() {}
+
+  async deleteById() {}
+}
