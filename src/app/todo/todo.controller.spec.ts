3b119dde1dcf78246b132788dff7f4af772c4180
Gabriel Brandao <gabrielbrandaosales@hotmail.com>
2025-05-05 09:32:22 -0300

    new: creation tests

--- src/app/todo/todo.controller.spec.ts
+++ src/app/todo/todo.controller.spec.ts
@@ -1,13 +1,17 @@
+/* eslint-disable @typescript-eslint/unbound-method */
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
 import { TodoEntity } from './entity/todo.entity';
+import { CreateTodoDTO } from './dto/create-todo.dto';
 
 const todoEntityList: TodoEntity[] = [
   new TodoEntity({ id: '1', task: 'task-1', isDone: 0 }),
   new TodoEntity({ id: '2', task: 'task-2', isDone: 0 }),
   new TodoEntity({ id: '3', task: 'task-3', isDone: 0 }),
 ];
+
+const newTodoEntity = new TodoEntity({ task: 'new-task', isDone: 0 });
 
 describe('TodoController', () => {
   let todoController: TodoController;
@@ -21,7 +25,7 @@ describe('TodoController', () => {
           provide: TodoService,
           useValue: {
             findAll: jest.fn().mockResolvedValue(todoEntityList),
-            create: jest.fn(),
+            create: jest.fn().mockResolvedValue(newTodoEntity),
             findOneOrFail: jest.fn(),
             update: jest.fn(),
             deleteById: jest.fn(),
@@ -56,6 +60,30 @@ describe('TodoController', () => {
 
       //Assert
       await expect(todoController.index()).rejects.toThrow('');
+    });
+  });
+
+  describe('create', () => {
+    it('should create a new todo item successfully', async () => {
+      //Arrange
+      const body: CreateTodoDTO = { task: 'new-task', isDone: 0 };
+
+      //Act
+      const result = await todoController.create(body);
+
+      //Assert
+      expect(result).toEqual(newTodoEntity);
+      expect(todoService.create).toHaveBeenCalledTimes(1);
+      expect(todoService.create).toHaveBeenCalledWith(body);
+    });
+
+    it('should throw an exception', async () => {
+      // Arrange
+      const body: CreateTodoDTO = { task: 'new-task', isDone: 0 };
+      jest.spyOn(todoService, 'create').mockRejectedValueOnce(new Error());
+
+      //Assert
+      await expect(todoController.create(body)).rejects.toThrow('');
     });
   });
 });
