1052004059eb59797f2f284392a562add4138cf3
Gabriel Brandao <gabrielbrandaosales@hotmail.com>
2025-05-06 21:14:04 -0300

    new: findAll tests created

--- src/app/todo/todo.service.spec.ts
+++ src/app/todo/todo.service.spec.ts
@@ -1,18 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodoService } from './todo.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { TodoEntity } from './entity/todo.entity';
+
+const todoEntityList: TodoEntity[] = [
+  new TodoEntity({ task: 'task-1', isDone: 0 }),
+  new TodoEntity({ task: 'task-2', isDone: 0 }),
+  new TodoEntity({ task: 'task-3', isDone: 0 }),
+];
 
 describe('TodoService', () => {
-  let service: TodoService;
+  let todoService: TodoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TodoService],
+      providers: [
+        TodoService,
+        {
+          provide: getRepositoryToken(TodoEntity),
+          useValue: {
+            find: jest.fn().mockResolvedValue(todoEntityList),
+            findOneOrFail: jest.fn(),
+            create: jest.fn(),
+            merge: jest.fn(),
+            save: jest.fn(),
+            softDelete: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<TodoService>(TodoService);
+    todoService = module.get<TodoService>(TodoService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(todoService).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return a todo list entity successfully', async () => {
+      // Act
+      const result = await todoService.findAll();
+
+      // Assert
+      expect(result).toEqual(todoEntityList);
+    });
   });
 });
