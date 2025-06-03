e4a51876e708d6aab5c44759d3107d24d3888ee1
Gabriel Brandao <gabrielbrandaosales@hotmail.com>
2025-06-03 18:42:26 -0300

    :sparkles: feat: new task component done

--- frontend/src/Components/NewTask/index.tsx
+++ frontend/src/Components/NewTask/index.tsx
@@ -1,28 +1,38 @@
 import { GoTrash } from 'react-icons/go';
 import './styles.css';
+import { useState } from 'react';
 
 const NewTask = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
   return (
-    <div className="checkbox-wrapper-15 item">
-      <input
-        className="inp-cbx"
-        id="cbx-15"
-        type="checkbox"
-        style={{ display: 'none' }}
-      />
-      <label className="cbx" htmlFor="cbx-15">
-        <span>
-          <svg width="12px" height="9px" viewBox="0 0 12 9">
-            <polyline points="1 5 4 8 11 1"></polyline>
-          </svg>
-        </span>
-        <span>To-do</span>
+    <div className="item">
+      <div className="checkbox-wrapper-18">
+        <div className="round">
+          <input
+            type="checkbox"
+            id="checkbox-18"
+            onChange={handleCheckboxChange}
+          />
+          <label htmlFor="checkbox-18"></label>
+        </div>
+      </div>
+      <label
+        className={
+          isChecked ? `task-description lineThrough` : `task-description`
+        }>
+        Integer urna interdum massa libero auctor neque turpis turpis semper.
+        Duis vel sed fames integer.
       </label>
       <button
         aria-label="Excluir tarefa"
         title="Excluir tarefa"
         className="btn-trash">
-        <GoTrash size={14} />
+        <GoTrash size={16} />
       </button>
     </div>
   );
